f3dbd7eb92460c8518e6cf849363cc700c8cb9e2
Nonso24 <52797761+Nonso24@users.noreply.github.com>
2020-09-28 22:43:32 +0100

    store information Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -27,7 +29,32 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
 
+        public void createMapIntent(View view) {
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            if (mapIntent !=null) {
+                mapIntent.resolveActivity(getPackageManager());
+                startActivity(mapIntent);
+            }
+        }
+
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view) {
+        Uri uri = Uri.parse("tel:0123456789");
+
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, uri);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        if (phoneIntent !=null) {
+            phoneIntent.resolveActivity(getPackageManager());
+            startActivity(phoneIntent);
+        }
+
+
+
+    }
 }
